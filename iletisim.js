d4a4268109284d72769e2c07d528f562fff84775
keremwq <keremgesme@gmail.com>
2024-05-18 12:27:48 +0300

    ninth commit

---
+++ iletisim.js
@@ -0,0 +1,44 @@
+const button = document.getElementById("button-submit")
+const buttonReset = document.getElementById("button-reset")
+const form = document.getElementById("myForm")
+
+button.addEventListener("click", () => {
+    ControlText()
+})
+
+buttonReset.addEventListener("click", () => {
+    form.reset()
+})
+
+function ControlText() {
+    let text = document.getElementById("text").value
+    let email = document.getElementById("email").value
+    let message = document.getElementById("message").value
+    if (text.trim() === "") {
+        alert("İsim kısmı boş.")
+        return
+    }
+    else if (email.trim() === "") {
+        alert("Email kısmı boş")
+        return
+    }
+    else if (message.trim() === "") {
+        alert("Mesaj kısmı boş")
+        return
+    }
+    else if (!email.includes("@")) {
+        alert("Geçersiz email.")
+        return
+    }
+
+    let etIndex = email.indexOf("@")
+    let emailPart = email.substring(etIndex, email.length)
+    if (!emailPart.includes(".")) {
+        alert("Geçersiz email.")
+        return
+    }
+
+    form.submit()
+
+}
+
--- iletisim.php
+++ iletisim.php
@@ -1,9 +1,7 @@
 <?php
-if(isset($_POST["submit"])) {
     $name = $_POST['name'];
     $mail = $_POST['mail'];
     $message = $_POST['message'];
-}
 ?>
 
 <!DOCTYPE html>
